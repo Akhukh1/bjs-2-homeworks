9b5a1432864bff38e332a5822128f050c2134c30
Aleks Khukh <aleks.khukh@gmail.com>
2022-12-05 19:04:34 +0300

    Задание 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,24 @@
 function compareArrays(arr1, arr2) {
-  
+
+  if (arr1.length === arr2.length && 
+    arr1.every((element, index) => element === arr2[index])) {
+    return true;
+  } else {
+    return false;
+  }
+
 }
 
 function getUsersNamesInAgeRange(users, gender) {
   
+  let averageAge = users.filter(user => user.gender === gender).map(genderArr => genderArr.age).reduce((acc, item, index, arr) => {
+    acc += item;
+    if (index === arr.length - 1) {
+      return acc / arr.length
+    }
+    return acc;
+  }, 0);
+
+  return averageAge;
+
 }
